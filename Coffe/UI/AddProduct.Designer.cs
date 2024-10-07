5b2339180cf669caeb3b73c794ef05a7aa6af4f7
Negin baziar <Negin.baziar1382@gmail.com>
2024-10-07 14:42:53 +0330

    product list

--- Coffe/UI/AddProduct.Designer.cs
+++ Coffe/UI/AddProduct.Designer.cs
@@ -44,22 +44,25 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Location = new System.Drawing.Point(18, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 31);
+            this.label1.Size = new System.Drawing.Size(277, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Product Name:";
             // 
             // productName
             // 
-            this.productName.Location = new System.Drawing.Point(205, 49);
+            this.productName.Location = new System.Drawing.Point(308, 75);
+            this.productName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.productName.Multiline = true;
             this.productName.Name = "productName";
-            this.productName.Size = new System.Drawing.Size(221, 31);
+            this.productName.Size = new System.Drawing.Size(330, 46);
             this.productName.TabIndex = 1;
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
@@ -67,18 +70,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 114);
+            this.label2.Location = new System.Drawing.Point(18, 175);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(177, 31);
+            this.label2.Size = new System.Drawing.Size(264, 46);
             this.label2.TabIndex = 3;
             this.label2.Text = "Product Price:";
             // 
             // productPrice
             // 
             this.productPrice.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.productPrice.Location = new System.Drawing.Point(205, 114);
+            this.productPrice.Location = new System.Drawing.Point(308, 175);
+            this.productPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.productPrice.Name = "productPrice";
-            this.productPrice.Size = new System.Drawing.Size(221, 27);
+            this.productPrice.Size = new System.Drawing.Size(332, 37);
             this.productPrice.TabIndex = 4;
             this.productPrice.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
@@ -86,9 +91,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 191);
+            this.label3.Location = new System.Drawing.Point(18, 294);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 31);
+            this.label3.Size = new System.Drawing.Size(185, 46);
             this.label3.TabIndex = 6;
             this.label3.Text = "Materials:";
             // 
@@ -96,9 +102,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.button1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(173, 383);
+            this.button1.Location = new System.Drawing.Point(260, 589);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 50);
+            this.button1.Size = new System.Drawing.Size(219, 77);
             this.button1.TabIndex = 7;
             this.button1.Text = "Confirm";
             this.button1.UseVisualStyleBackColor = false;
@@ -106,17 +113,18 @@
             // 
             // materialsPlace
             // 
-            this.materialsPlace.Location = new System.Drawing.Point(205, 171);
+            this.materialsPlace.Location = new System.Drawing.Point(308, 263);
+            this.materialsPlace.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.materialsPlace.Name = "materialsPlace";
-            this.materialsPlace.Size = new System.Drawing.Size(221, 187);
+            this.materialsPlace.Size = new System.Drawing.Size(332, 288);
             this.materialsPlace.TabIndex = 9;
             // 
             // AddProduct
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(453, 445);
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(680, 685);
             this.Controls.Add(this.materialsPlace);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -124,6 +132,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.productName);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AddProduct";
             this.Text = "AddProduct";
             this.Load += new System.EventHandler(this.AddProduct_Load);
